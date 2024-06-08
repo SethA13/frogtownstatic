bbe8b4045389c59f553a574e5b77b68c67124f29
SethA13 <seth1309@gmail.com>
2024-06-07 18:44:56 -0600

    deck update

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Frogtown dev";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # insert desired packages here
+        tmux
+      ];
+    };
+  };
+}
